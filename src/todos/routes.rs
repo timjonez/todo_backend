6c936c3980ee2ca4a3ba7dcfc11e4bd26682c047
Tim Jones <timjonez@protonmail.com>
2023-02-19 22:19:02 -0800

    try to get data from db

--- src/todos/routes.rs
+++ src/todos/routes.rs
@@ -1,6 +1,13 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
+use crate::db::App;
 
 #[get("/")]
-pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+pub async fn get_list(db: web::Data<App>) -> impl Responder {
+    let query = "select * from todo";
+    let res = &db.db.execute(query, &db.session, None, false)
+        .await
+        .unwrap();
+
+
+    HttpResponse::Ok().json(res)
 }
