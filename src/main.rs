19ae2aec3c593e36a6a2b4f37c2cfebfac27f13a
Tim Jones <timjonez@protonmail.com>
2023-02-21 21:59:45 -0800

    remove app from context

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-use todo_backend::db::setup;
 use actix_web::{App, HttpServer};
 use todo_backend::todos::get_list;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let db = setup().await;
-
     HttpServer::new(|| {
         App::new()
             .service(get_list)
-            .app_data(db.clone())
     })
     .bind(("127.0.0.1", 8000))?
     .run()
