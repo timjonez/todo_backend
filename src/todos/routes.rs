6143e450c31e50a7c356db553b64cc7b10134793
Tim Jones <timjonez@protonmail.com>
2023-02-22 22:07:28 -0800

    add endpoint to get todo by id

--- src/todos/routes.rs
+++ src/todos/routes.rs
@@ -1,7 +1,7 @@
-use actix_web::{get, HttpResponse, Responder};
+use actix_web::{get, web, HttpResponse, Responder};
 use crate::db::setup_db;
 
-#[get("/")]
+#[get("/todos")]
 pub async fn get_list() -> impl Responder {
     let (db, session) = setup_db().await;
 
@@ -11,4 +11,23 @@ pub async fn get_list() -> impl Responder {
         .unwrap();
 
     HttpResponse::Ok().json(res.first().unwrap().output().unwrap())
+}
+
+#[get("/todos/{id}")]
+pub async fn get_todo(path: web::Path<String>) -> impl Responder {
+    let id = path.into_inner();
+
+    let (db, session) = setup_db().await;
+
+    let query = format!("select * from todo where id = 'todo:{}'", id);
+    let res = db.execute(&query, &session, None, false)
+        .await
+        .unwrap();
+
+    let result = res.first()
+        .unwrap()
+        .output()
+        .unwrap();
+
+    HttpResponse::Ok().json(result)
 }
