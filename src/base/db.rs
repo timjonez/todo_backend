9fc4e63acc252ca9491c5d076550ac9ee7e96991
Tim Jones <timjonez@protonmail.com>
2023-03-09 21:58:53 -0800

    move db into core

---
+++ src/base/db.rs
@@ -0,0 +1,18 @@
+use surrealdb::{Datastore, Session};
+
+pub async fn setup_db() -> (Datastore, Session) {
+    let db = get_database().await;
+    let session = get_session().await;
+
+    (db, session)
+}
+
+pub async fn get_database() -> Datastore {
+    Datastore::new("file:/home/tim/Coding/Projects/rusty_todo/todo_backend/test.db")
+        .await
+        .unwrap()
+}
+
+pub async fn get_session() -> Session {
+    Session::for_kv().with_ns("test_ns").with_db("test_db")
+}
