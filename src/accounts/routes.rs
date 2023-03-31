f2bf06d3e361a27bce488c611b14a9784bcaa238
Tim Jones <timjonez@protonmail.com>
2023-03-31 15:27:08 -0700

    add logic for the login endpoint

--- src/accounts/routes.rs
+++ src/accounts/routes.rs
@@ -1,9 +1,10 @@
-use actix_web::{ post, web, HttpResponse, Responder};
-use crate::base::setup_db;
 use crate::accounts::models::User;
+use crate::base::setup_db;
+use crate::base::{check_token, get_token};
+use actix_web::{post, web, HttpResponse, Responder};
 use email_address::EmailAddress;
+use serde_json::json;
 use std::collections::HashMap;
-
 
 #[derive(serde::Deserialize)]
 pub struct CreateUserPayload {
@@ -12,20 +13,26 @@ pub struct CreateUserPayload {
     pub name: String,
 }
 
-
 #[post("/users/")]
 pub async fn create_user(payload: web::Json<CreateUserPayload>) -> impl Responder {
     let (db, session) = setup_db().await;
-    let user = User::new(payload.email.to_string(), payload.password.clone(), payload.name.clone()).unwrap();
+    let user = User::new(
+        payload.email.to_string(),
+        payload.password.clone(),
+        payload.name.clone(),
+    )
+    .unwrap();
 
     let query = "CREATE user SET name = $name, email = $email, password = $password";
     let vars = [
         ("name".into(), user.name.into()),
         ("email".into(), user.email.to_string().into()),
-        ("password".into(), user.password.password().into())
-    ].into();
+        ("password".into(), user.password.password().into()),
+    ]
+    .into();
 
-    let res = db.execute(&query, &session, Some(vars), false)
+    let _res = db
+        .execute(&query, &session, Some(vars), false)
         .await
         .unwrap();
 
@@ -45,18 +52,32 @@ pub async fn user_login(payload: web::Json<LoginPayload>) -> impl Responder {
     let query = "SELECT * FROM user where email = $email";
     let vars = [("email".into(), payload.email.to_string().into())].into();
     let res = match db.execute(query, &session, Some(vars), false).await {
-        Err(..) => { return HttpResponse::BadRequest().finish()},
-        Ok(r) => r 
+        Err(..) => return HttpResponse::BadRequest().finish(),
+        Ok(r) => r,
     };
     let user: User = match res.len() {
-        1 => {
-            User::from(res.first().expect("err1").result.as_ref().expect("err2").clone())
-        },
-        _ => { return HttpResponse::BadRequest().json(HashMap::from([("error", "Invalid credentials")])) }
+        1 => User::from(
+            res.first()
+                .expect("err1")
+                .result
+                .as_ref()
+                .expect("err2")
+                .clone(),
+        ),
+        _ => {
+            return HttpResponse::BadRequest()
+                .json(HashMap::from([("error", "Invalid credentials")]))
+        }
     };
 
     match &user.password.check_password(&payload.password) {
-        true => { HttpResponse::Ok().json(user) },
-        false => { HttpResponse::Unauthorized().finish() }
+        false => HttpResponse::Unauthorized().finish(),
+        true => {
+            let token = get_token(user.id, user.email);
+            println!("> {:?}", token);
+            let payload = check_token(token.clone()).unwrap();
+            println!(">>> {:?}", payload);
+            HttpResponse::Ok().json(json!({ "token": token }))
+        }
     }
 }
