dfe570acb489037b704f8cc49fba00b59093f949
Tim Jones <timjonez@protonmail.com>
2023-03-30 21:01:03 -0700

    wrap endpoints with the auth middleware

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use actix_web::{App, HttpServer};
-use todo_backend::accounts::User;
-use todo_backend::todos::{get_list, get_todo};
 use todo_backend::accounts::{create_user, user_login};
+use todo_backend::base::Auth;
+use todo_backend::todos::{get_list, get_todo};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_list).service(get_todo).service(create_user).service(user_login))
-        .bind(("127.0.0.1", 8000))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Auth)
+            .service(create_user)
+            .service(user_login)
+            .service(get_list)
+            .service(get_todo)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
